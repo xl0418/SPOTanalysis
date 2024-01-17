f98543599aef54e8e5ac03651177df9eb758a2c3
Liang Xu <xl0418@gmail.com>
2024-01-17 13:44:23 -0800

    updated

---
+++ 05-MaxG_cluster_SPOT_1hit_FL.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import multiprocess as mp
+# set the threshold for the relative abundance
+a_thd = 0
+
+# read the geo meta data
+geo_meta = pd.read_csv('data/SPOT_CTD_Nutrients_BP_5Depths.csv')
+# remove the first two rows
+geo_meta = geo_meta.iloc[2:,:]
+
+def generate_df(ii):
+    # read the geo meta data
+    geo_meta = pd.read_csv('data/SPOT_CTD_Nutrients_BP_5Depths.csv')
+    # remove the first two rows
+    geo_meta = geo_meta.iloc[2:, :]
+    # read the tag2genome data
+    tag2genome = pd.read_csv('data/SPOT_Prokaryotic16S_ASV_dna-sequences_BLASToutput.tsv', sep='\t', header=None)
+
+    # read the tag2sample data relative abundance data
+    file_relative_a = 'data/SPOT_FreeLivingProkaroytes_ASV_2005_2018_5depths.csv'
+    tag2sample = pd.read_csv(file_relative_a, sep=',')
+
+    # read the predicted growth rate data
+    pred_g = pd.read_csv('data/SPOT_EstG_genome_full.tsv', sep='\t')
+
+    sample1, sample2, sample3 = geo_meta.iloc[ii, 0].split('/')
+    depthbin = geo_meta.iloc[ii, 1]
+    sample = '20' + sample3 + '_' + sample1 + '_' + sample2 + '_' + depthbin + '_' + 'FL'
+
+    depth = geo_meta['depth'].iloc[ii]
+    # Lat = geo_meta['Latitude'][ii]
+    temp = geo_meta['CTDTemp'].iloc[ii]
+    batch_data = pd.DataFrame(columns=['sample',
+                                    'depth',
+                                    'temp',
+                                    'tag',
+                                    'genomeid',
+                                    'relative_a',
+                                    'maxg',
+                                   'LowerCI',
+                                   'UpperCI',
+                                   'dCUB',
+                                   'nHE'])
+    # if sample is not in relative abundance data, skip
+    if sample in tag2sample.columns:
+        present_tags = tag2sample[tag2sample[sample]>a_thd]
+        for pres_tag in present_tags.index:
+            relative_a_tag = present_tags[present_tags.index==pres_tag][sample].values[0]
+            genomes = tag2genome[tag2genome.iloc[:,0]==pres_tag].iloc[:,1]
+            if genomes.__len__() > 0:
+                for genome in genomes.values:
+                    matched_genome = pred_g[pred_g['genomeid']==genome]
+                    if matched_genome.__len__() > 0:
+                        pred_g_genome = pred_g[pred_g['genomeid']==genome]['d'].values[0]
+                        lowerci = pred_g[pred_g['genomeid']==genome]['LowerCI'].values[0]
+                        upperci = pred_g[pred_g['genomeid']==genome]['UpperCI'].values[0]
+                        dCUB = pred_g[pred_g['genomeid']==genome]['dCUB'].values[0]
+                        nHE = pred_g[pred_g['genomeid']==genome]['nHE'].values[0]
+                        temp_df = pd.DataFrame({'sample':sample,
+                                                        'depth':depth,
+                                                        'temp':temp,
+                                                        'tag':pres_tag,
+                                                      'genomeid':genome,
+                                                        'relative_a':relative_a_tag,
+                                                       'maxg':pred_g_genome,
+                                                         'LowerCI':lowerci,
+                                                            'UpperCI':upperci,
+                                                            'dCUB':dCUB,
+                                                            'nHE':nHE
+                                                }, index=[0])
+
+                        batch_data = pd.concat([batch_data, temp_df], ignore_index=True)
+                        # if want to get estimates for all genomes, remove the break
+                        break
+    return batch_data
+
+with mp.Pool(processes = (16)) as pool:
+    data = pool.map(generate_df, [ii for ii in range(geo_meta.__len__())])
+
+full_data = pd.concat(data, axis=0)
+
+# save the full data
+full_data.to_csv('data/SPOT_1hits_FL.csv', index=False)
