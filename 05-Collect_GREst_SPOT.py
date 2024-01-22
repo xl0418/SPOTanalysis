4e3d9cf0158c0c97fca8add82d3886933c76b0cd
Liang Xu <xl0418@gmail.com>
2024-01-22 09:16:40 -0800

    bug fixed

--- 05-Collect_GREst_SPOT.py
+++ 05-Collect_GREst_SPOT.py
@@ -20,7 +20,7 @@ for genomeid in file.iloc[:,0].unique():
         with open(est_temp_file) as f:
             data = json.load(f)
             data['genomeid'] = genomeid
-            data_temp_df = pd.concat([data_df, pd.DataFrame(data, index=[0])], ignore_index=True)
+            data_temp_df = pd.concat([data_temp_df, pd.DataFrame(data, index=[0])], ignore_index=True)
 
 data_df.to_csv("data/SPOT_EstG_genome_sameT.tsv", sep='\t', index=False)
 data_temp_df.to_csv("data/SPOT_EstG_genome_variableT.tsv", sep='\t', index=False)
